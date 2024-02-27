18281cd568596a043d4cf18c61bdd3704eea1fdc
v-gaonkar <vidyashreegaonkarylp@gmail.com>
2024-02-27 18:21:24 +0530

    Fifth Assignment using AngularJS

--- Assignment5/JS/toggleList.js
+++ Assignment5/JS/toggleList.js
@@ -1,11 +1,9 @@
 angular.module('movieSearch.toggleList', [])
-.controller('ToggleList', ['$scope', function ($mdSidenav) {
-    var vm = this;
-    vm.toggleLeft = buildToggler('left');
-
-    function buildToggler(componentId) {
-        return function() {
-            $mdSidenav(componentId).toggle();
-        };
-    }
+.controller('ToggleList', ['$scope', '$mdSidenav', '$mdMedia', function ($scope, $mdSidenav, $mdMedia) {
+    $scope.isSidenavLockedOpen = function() {
+        return $mdMedia('gt-sm') && $mdSidenav('left').isLockedOpen();
+    };
+    $scope.toggleLeft = function() {
+        $mdSidenav('left').toggle();
+    };
 }]);
